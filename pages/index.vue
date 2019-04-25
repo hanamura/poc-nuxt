7d958a5e24afcb5a1cabc0c5d1377834e6a5b6a7
Taro Hanamura <m@hanamurataro.com>
2019-04-25 19:56:30 +0900

    Display index

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <h1>Home</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.slug">
+        <nuxt-link :to="`/news/${post.slug}`">
+          {{ post.title }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  asyncData() {
+    const context = require.context('~/content/news/posts', false, /^.*.yml$/)
+    return { posts: context.keys().map(key => context(key)) }
+  }
+}
+</script>
